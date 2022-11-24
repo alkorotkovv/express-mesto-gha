82b9b38e5445b0daa4fb3fb7df3668d1f0629ff5
Alex <al.korotkov@intranet-sintek.net>
2022-11-24 13:07:35 +0300

    add auth middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const {
+  login,
+  createUser,
+} = require('./controllers/user');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -19,6 +23,9 @@ const addId = (req, res, next) => {
 app.use(addId);
 app.use('/', require('./routes/user'));
 app.use('/', require('./routes/card'));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Такого роута не существует' });
--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,10 @@ const router = require('express').Router();
 const {
   getUsers,
   getUserById,
-  createUser,
   updateMeInfo,
   updateMeAvatar,
 } = require('../controllers/user');
 
-router.post('/users', createUser);
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.patch('/users/me', updateMeInfo);
