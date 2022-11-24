fd571e9e3e487c9668b5be85f28fddf9bff1283e
Alex <al.korotkov@intranet-sintek.net>
2022-11-24 14:40:35 +0300

    add avatar pattern

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
+const { celebrate, Joi, errors} = require('celebrate');
 const {
   login,
   createUser,
@@ -29,7 +29,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(new RegExp("^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$")),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
