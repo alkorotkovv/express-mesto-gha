604a4ea220dbb29ef68aa0058b4f7235a06eb9d6
Alex <al.korotkov@intranet-sintek.net>
2022-11-25 11:52:06 +0300

    edit user controller

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,6 @@ const BadRequestError = require('../errors/BadRequestError');
 const ConflictError = require('../errors/ConflictError');
 const NotFoundError = require('../errors/NotFoundError');
 const ServerError = require('../errors/ServerError');
-const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports.getMe = (req, res, next) => {
   console.log(req);
@@ -16,7 +15,7 @@ module.exports.getMe = (req, res, next) => {
       else next(new NotFoundError('Пользователь с таким id не найден'));
     })
     .catch((err) => {
-      if ((err.name === 'CastError') || (err.name === 'ValidationError')) {
+      if (err.name === 'CastError') {
         next(new BadRequestError('Переданы некорректные данные'));
       } else {
         next(new ServerError('Произошла ошибка'));
@@ -40,7 +39,7 @@ module.exports.getUserById = (req, res, next) => {
       else next(new NotFoundError('Пользователь с таким id не найден'));
     })
     .catch((err) => {
-      if ((err.name === 'CastError') || (err.name === 'ValidationError')) {
+      if (err.name === 'CastError') {
         next(new BadRequestError('Переданы некорректные данные'));
       } else {
         next(new ServerError('Произошла ошибка'));
@@ -62,7 +61,7 @@ module.exports.createUser = (req, res, next) => {
       res.send({ data: user });
     })
     .catch((err) => {
-      if ((err.name === 'CastError') || (err.name === 'ValidationError')) {
+      if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные'));
       } else if (err.code === 11000) {
         next(new ConflictError('Такой пользователь уже существует'));
@@ -112,5 +111,5 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
     })
-    .catch(() => next(new UnauthorizedError('Неверный логин или пароль')));
+    .catch((err) => next(err));
 };
