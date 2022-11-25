b9c68ed8911d90d3300bff8e66aa50205d2e693f
Alex <al.korotkov@intranet-sintek.net>
2022-11-25 12:29:29 +0300

    refactoring structure

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { constants } = require('http2');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
-const {
-  login,
-  createUser,
-} = require('./controllers/user');
-const auth = require('./middlewares/auth');
-const NotFoundError = require('./errors/NotFoundError');
 
-const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
-const emailRegex = /^([a-zA-Z0-9_.-]+)@([a-z0-9_.-]+)\.([a-z.]{2,6})$/;
+const routes = require('./routes');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
@@ -19,38 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().pattern(emailRegex),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(urlRegex),
-    email: Joi.string().required().pattern(emailRegex),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-
-app.use(auth);
-app.use('/', require('./routes/user'));
-app.use('/', require('./routes/card'));
-
-app.use((req, res, next) => {
-  next(new NotFoundError('Такого роута не существует'));
-});
-
-app.use(errors());
-
-app.use((err, req, res, next) => {
-  const status = err.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
-  const message = err.message || 'Неизвестная ошибка';
-  res.status(status).send({ message });
-  next();
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
