b4e59cfeaf9b854d0374001d9dfd4aaf20ac0b3b
Alex <al.korotkov@intranet-sintek.net>
2022-11-25 11:18:24 +0300

    refactoring

--- models/user.js
+++ models/user.js
@@ -42,12 +42,12 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return new UnauthorizedError('Неправильные почта или пароль');
+        return Promise.reject(new Error('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return new UnauthorizedError('Неправильные почта или пароль');
+            return Promise.reject(new Error('Неправильные почта или пароль'));
           }
           return user; // теперь user доступен
         });
