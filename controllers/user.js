fb7e049f7caddd3bc085a9a594bde992494c151f
Alex <al.korotkov@intranet-sintek.net>
2022-12-02 10:02:18 +0300

    refactoring

--- controllers/user.js
+++ controllers/user.js
@@ -107,6 +107,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      console.log(user._id);
       // аутентификация успешна! пользователь в переменной user
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
