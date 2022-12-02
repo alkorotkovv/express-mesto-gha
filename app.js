d564924773a3aa2d683b33f27fad18af408fa90c
Alex <al.korotkov@intranet-sintek.net>
2022-12-02 07:14:30 +0300

    add cors

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const cors = require('cors');
 const routes = require('./routes');
 const errorsHandler = require('./middlewares/errors');
-const cors = require('./middlewares/cors');
+//const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
