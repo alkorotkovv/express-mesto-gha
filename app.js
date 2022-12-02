24423426d1d2a60ac8a46256f50df2516cbdeafe
Alex <al.korotkov@intranet-sintek.net>
2022-12-02 07:21:11 +0300

    add cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
-app.use(cors);
+app.use(cors());
 app.use(routes);
 app.use(errorsHandler);
 
