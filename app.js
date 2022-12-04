6e9b8a1beb318580508c06abb0eeda7ac52080e4
Alex <al.korotkov@intranet-sintek.net>
2022-12-04 11:21:51 +0300

    refactoring

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const routes = require('./routes');
 const errorsHandler = require('./middlewares/errors');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
