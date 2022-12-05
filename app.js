4abc27647cbdf8516b51684b4b47b0b100d9c99a
Alex <al.korotkov@intranet-sintek.net>
2022-12-05 09:08:29 +0300

    edit errors handler

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
 app.use(cors());
 app.use(routes);
-app.use(errorsHandler);
 
 app.use(errors());
+app.use(errorsHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
